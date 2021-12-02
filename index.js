70f9bc4c9e0613382b6bbe28c30b51af06a4b589
ahmed sayed <ahmedsayedmohammed93@gmail.com>
2021-12-02 21:28:28 +0200

    add start flag

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function sayHi()
 
 
 
-
+console.log("app start....");
 console.log(`the value of index 4 in fibonanci series is ${fibonanci(4)}`);
 console.log(`the factorial of  4  is ${factorial(4)}`);
 console.log(`the power 2 of  4  is ${power(4)}`);
