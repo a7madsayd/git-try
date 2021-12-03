77e4a6cc27a1101a296356d2dd1a50f4ebfe939c
ahmed sayed <ahmedsayedmohammed93@gmail.com>
2021-12-03 06:25:31 +0200

    add 5 power 5 to result

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ console.log('the results is : ');
 console.log(`the value of index 4 in fibonanci series is ${fibonanci(4)}`);
 console.log(`the factorial of  4  is ${factorial(4)}`);
 console.log(`the power 2 of  4  is ${power(4 ,2)}`);
+console.log(`the power 5 of  5  is ${power(5 ,5)}`);
 
 console.log(sayGoodBye());
 
