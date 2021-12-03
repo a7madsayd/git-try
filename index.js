87023270ef27dfb7e6962305255d1f60928fd9f2
ahmed sayed <ahmedsayedmohammed93@gmail.com>
2021-12-03 02:35:55 +0200

    refactore index.js

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-const  fibonanci  = require("./fibonanci")
-const  factorial  = require("./factorial")
-const  power  = require("./power")
+const fibonanci = require("./fibonanci");
+const factorial = require("./factorial");
+const power = require("./power");
 
-function sayHi()
-{
-    return 'Hi!';
+function sayHi() {
+  return "Hi!";
 }
 
-function sayGoodBye()
-{
-    return 'Good Bye!';
+function sayGoodBye() {
+  return "Good Bye!";
 }
-
-
-
 
 console.log("app start....");
 console.log(sayHi());
 console.log(`the value of index 4 in fibonanci series is ${fibonanci(4)}`);
 console.log(`the factorial of  4  is ${factorial(4)}`);
 console.log(`the power 2 of  4  is ${power(4)}`);
-
 console.log(sayGoodBye());
-
